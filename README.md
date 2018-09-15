d2b867905e7755d02143600ef7edd3d6857522a0
nguyenvancaoky <nguyenvancaoky@gmail.com>
2018-09-15 11:43:29 +0700

    add

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+# Ad
 # blogtruyen.com
 Giao diện trực quan cho blogtruyen.com
---
+++ push.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd E:\blogtruyen.com
+git add .
+git commit -m "add"
+git push origin master
+echo Press Enter...
+read
